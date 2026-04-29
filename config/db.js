f4ca8f1eb326b037570f1245407fb8c693c859d9
shubhamdwivedi2910 <shubham.dwivedi2@gmail.com>
2026-04-29 15:36:49 +0530

    Postgress Add

--- config/db.js
+++ config/db.js
@@ -1,14 +1,36 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASS,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql',
+//     logging: false
+//   }
+// );
+
+// module.exports = sequelize;
+
+
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DB_NAME,          // database
+  process.env.DB_USER,          // user
+  process.env.DB_PASS,     // password
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: false
+    dialect: 'postgres',
+    port: process.env.DB_PORT,
+    logging: false,
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 );
 
